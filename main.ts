7cba416107afa23addcf2b10d0b9bd004a645255
ccosta-fbk <61448923+ccosta-fbk@users.noreply.github.com>
2020-04-04 20:01:44 +0200

    export function showFunFace()

--- main.ts
+++ main.ts
@@ -1,7 +1,17 @@
-basic.showLeds(`
-    . . . . .
-    . # . # .
-    . . # . .
-    # . . . #
-    . # # # .
-    `);
+/**
+ * MakeCode extension di test lamponi
+ */
+//% color=#009b5b icon="\uf1eb" block="Lamponi"
+namespace lamponi {
+
+    export function showFunFace() {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . # . .
+            # . . . #
+            . # # # .
+            `);
+
+    }
+}
